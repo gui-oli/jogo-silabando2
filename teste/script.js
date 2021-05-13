f3a31a529326749ec16d1abba7384abe485e08d7
gui-oli <guilherme.ubergui@gmail.com>
2021-05-13 15:14:31 -0300

    retornar se estiver errado

--- teste/script.js
+++ teste/script.js
@@ -1,12 +1,13 @@
 
-const cards = document.querySelectorAll('.card')
+const cards     = document.querySelectorAll('.card')
 const dropzones = document.querySelectorAll('.dropzone')
+const boards    = document.querySelectorAll('.board')
 
 /* our cards */
 cards.forEach(card => {
     card.addEventListener('dragstart', dragstart)
-    card.addEventListener('drag', drag)
-    card.addEventListener('dragend', dragend)
+    card.addEventListener('drag'     , drag     )
+    card.addEventListener('dragend'  , dragend  )
 })
 
 function dragstart() {
@@ -21,31 +22,58 @@ function drag() {
 function dragend() {
     //console.log('CARD: Stop drag')
     dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
-
+    
     this.classList.remove('is-dragging')
+   this.setAttribute('draggable', 'true')
+   
+   //document.getElementById('card1').removeAttribute('draggable',false)
+   //this.parentElement.setAttribute('ondrop', 'false')
+   const caixas = this.parentElement.className.substring(0,2)
+   const silaba = this.className.substring(0,2)
+   
+    console.log(this.className)
+    console.log(caixas)
+
+    if (silaba != caixas) {
+
+       console.log("Volta porque isso não é teu...")
+       document.getElementById("caixa").appendChild(this)
+      
+    }
 }
  /*place where we will drop cards */
  dropzones.forEach( dropzone => {
      dropzone.addEventListener('dragenter', dragenter)
-     dropzone.addEventListener('dragover', dragover)
+     dropzone.addEventListener('dragover' , dragover )
      dropzone.addEventListener('dragleave', dragleave)
-     dropzone.addEventListener('drop', drop)
+     dropzone.addEventListener('drop'     , drop     )
  })
 
  function dragenter() {
-    
+
  }
  function dragover() {
+    
+
     this.classList.add('over')
 
     // get dragging card
     const cardBeingDragged = document.querySelector('.is-dragging')
 
+   // alert(this.parentNode.className)
+
+   
     this.appendChild(cardBeingDragged)
+    
  }
  function dragleave() {
-    this.classList.remove('over');
+    this.classList.remove('over')
+    
+    
+    
 }
- function drop() {
-    this.classList.remove('over');
+function drop() {
+    console.log("klkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
+
+    
  }
