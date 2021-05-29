3ccd4ac5ad16351f2371e08c8d0fc149a654092e
gui-oli <guilherme.ubergui@gmail.com>
2021-05-29 15:22:50 -0300

    randon syllable and img

---
+++ silabas.js
@@ -0,0 +1,28 @@
+data = [{
+    nivel1: [
+    {
+      "palavra":"canudo",
+      "silaba": ["CA","NU","DO"],
+    },
+    {
+        "palavra":"banana",
+        "silaba": ["BA","NA","NA"],
+    },
+    {
+        "palavra":"caneta",
+        "silaba": ["CA","NE","TA"]
+    },
+    {
+        "palavra":"cana",
+        "silaba": ["CA","NA"]
+    },
+    {
+        "palavra":"abacate",
+        "silaba": ["A","BA","CA","TE"],
+    },
+    {
+        "palavra":"cachorro",
+        "silaba": ["CA","CHOR","RO"],
+    }
+]
+}]
--- teste/script.js
+++ teste/script.js
@@ -1,9 +1,40 @@
+const total = data[0].nivel1.length -1
+const random = Math.floor(Math.random()*(total-0+1)+0)
+const licao = data[0].nivel1[random]
+
+licao.silaba.forEach(s =>{
+    //board
+    var board = document.createElement('div');
+    board.classList.add('board')
+
+    document.querySelector("div.boards").appendChild(board);
+
+    //caixa
+    var caixa = document.createElement('div');
+    caixa.setAttribute('id', s);
+    caixa.classList.add(s);
+    caixa.classList.add('dropzone');
+    caixa.classList.add('estilo');
+    board.appendChild(caixa);
+
+    //silaba
+    var silaba = document.createElement('div');
+    silaba.textContent = s;
+    silaba.classList.add(s)
+    silaba.classList.add('card')
+    silaba.classList.add('silaba')
+    silaba.setAttribute('draggable', true);
+    document.getElementById("caixa").appendChild(silaba);
+  })
+
+  document.querySelector("img").setAttribute('src', "../imagens/"+licao.palavra+".jpg" );
+
+
 
 const cards     = document.querySelectorAll('.card')
 const dropzones = document.querySelectorAll('.dropzone')
 const boards    = document.querySelectorAll('.board')
 
-/* our cards */
 cards.forEach(card => {
     card.addEventListener('dragstart', dragstart)
     card.addEventListener('drag'     , drag     )
@@ -11,23 +42,21 @@ cards.forEach(card => {
 })
 
 function dragstart() {
-   // console.log('CARD: Start dragging')
    dropzones.forEach( dropzone => dropzone.classList.add('highlight'))
-
-   // esse cara faz card aparecer na caixa antes de solta
    this.classList.add('is-dragging')
 }
 function drag() {
 }
 function dragend() {
-    //console.log('CARD: Stop drag')
     dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
-    
     this.classList.remove('is-dragging')
-   this.setAttribute('draggable', 'true')
-   
-   const caixas = this.parentElement.className.substring(0,2)
-   const silaba = this.className.substring(0,2)
+    this.setAttribute('draggable', 'true')
+
+    //console.log(this.parentElement.className)
+    //console.log(this.className)
+
+    const caixas = this.parentElement.className.substring(0,5)
+    const silaba = this.className.substring(0,5)
 
     if (silaba != caixas) {
 
@@ -41,11 +70,11 @@ function dragend() {
             count ++
         }
      })
+     this.setAttribute('draggable', 'false')
      count == dropzones.length ? alert("Você é demais!!!") : ""
     }
     
 }
- /*place where we will drop cards */
  dropzones.forEach( dropzone => {
      dropzone.addEventListener('dragenter', dragenter)
      dropzone.addEventListener('dragover' , dragover )
@@ -57,22 +86,14 @@ function dragend() {
 
  }
  function dragover() {
-    
-
     this.classList.add('over')
-
-    // get dragging card
     const cardBeingDragged = document.querySelector('.is-dragging')
-
-   
     this.appendChild(cardBeingDragged)
-    
  }
  function dragleave() {
     this.classList.remove('over')
-    
 }
 function drop() {
-
-    
 }
+
+
